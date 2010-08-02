5b8c34254fb16f2cad1575b3f1051edd3d888a92
Anton Vaynshtok <avaynshtok@gmail.com>
2010-08-01 19:06:36 -0700

    holy shit

--- netstat.cpp
+++ netstat.cpp
@@ -9,10 +9,15 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/sysctl.h>
 
-#include "netstat.h"
+//#include "netstat.h"
+
+#include <sys/socketvar.h>
+#include "unpcb.h"
 
 #include <errno.h>
+#include <sys/socket.h>
 
 static	const char *const socktype[] =
 { "#0", "stream", "dgram", "raw", "rdm", "seqpacket" };
@@ -25,7 +30,7 @@ pcblist_sysctl(int type, char **bufp)
 	size_t	len;
 	char mibvar[sizeof "net.local.seqpacket.pcblist"];
 	
-	sprintf(mibvar, "net.local.%s.pcblist", socktype[type]);
+	sprintf(mibvar, "net.local.%s.pcblist64", socktype[type]);
 	
 	len = 0;
 	if (sysctlbyname(mibvar, 0, &len, 0, 0) < 0) {
@@ -46,42 +51,54 @@ pcblist_sysctl(int type, char **bufp)
 		free(buf);
 		return (-2);
 	}
+	
+	
+	printf("pcblist len: %i\n", len);
+	//exit(0);
+	
 	*bufp = buf;
 	return (0);
 }
 
-
-static void unixdomainpr(struct xunpcb *xunp, struct xsocket *so)
+static void unixdomainpr(struct xunpcb64 *xunp, struct xsocket64 *so)
 {
 	struct unpcb *unp;
+//	struct unpcb_compat *unp;
 	struct sockaddr_un *sa;
 	static int first = 1;
 	
-	unp = &xunp->xu_unp;
+	/*
+	unp = (unpcb*)xunp->xu_unpp;
 	if (unp->unp_addr)
 		sa = &xunp->xu_addr;
 	else
 		sa = (struct sockaddr_un *)0;
+	*/
+	sa = &xunp->xu_addr;
 	
 	if (first) {
 		printf("Active UNIX domain sockets\n");
 		printf(
-			   "%-8.8s %-6.6s %-6.6s %-6.6s %8.8s %8.8s %8.8s %8.8s Addr\n",
+			   "%-16.16s %-6.6s %-6.6s %-6.6s %16.16s %16.16s %16.16s %16.16s Addr\n",
 			   "Address", "Type", "Recv-Q", "Send-Q",
 			   "Inode", "Conn", "Refs", "Nextref");
 		first = 0;
 	}
-	printf("%8lx %-6.6s %6u %6u %8lx %8lx %8lx %8lx",
-	       (long)so->so_pcb, socktype[so->so_type], so->so_rcv.sb_cc,
-	       so->so_snd.sb_cc,
-	       (long)unp->unp_vnode, (long)unp->unp_conn,
-	       (long)LIST_FIRST(&unp->unp_refs), (long)LIST_NEXT(unp, unp_reflink));
 	/*
-	if (sa)
-		printf(" %.*s",
-			   (int)(sa->sun_len - offsetof(struct sockaddr_un, sun_path)),
-			   sa->sun_path);
-	*/
+	//printf("%8lx %-6.6s %6u %6u %8lx %8lx %8lx %8lx",
+	//      (long)so->so_pcb, socktype[so->so_type], so->so_rcv.sb_cc,
+	//       so->so_snd.sb_cc,
+	//       (long)unp->unp_vnode, (long)unp->unp_conn,
+	//       (long)LIST_FIRST(&unp->unp_refs), (long)LIST_NEXT(unp, unp_reflink));
+	printf("%8lx %-6.6s %6u",  (long)so->so_pcb, socktype[so->so_type], so->so_rcv.sb_cc);
+	 */
+	printf("%16lx %-6.6s %6u %6u %16lx %16lx %16lx %16lx",
+	       (long)xunp->xu_unpp, socktype[so->so_type], so->so_rcv.sb_cc,
+	       so->so_snd.sb_cc,
+	       (long)xunp->xunp_vnode, (long)xunp->xunp_conn,
+	       (long)xunp->xunp_refs, (long)xunp->xunp_reflink.le_next);
+	
+	
 	putchar('\n');
 }
 
@@ -91,9 +108,9 @@ int main (int argc, const char * argv[])
 {
 	char 	*buf;
 	int	ret, type;
-	struct	xsocket *so;
+	struct	xsocket64 *so;
 	struct	xunpgen *xug, *oxug;
-	struct	xunpcb *xunp;
+	struct	xunpcb64 *xunp;
 	
 	for (type = SOCK_STREAM; type <= SOCK_SEQPACKET; type++) {
 		//		if (live)
@@ -107,16 +124,26 @@ int main (int argc, const char * argv[])
 		if (ret < 0)
 			return 0;
 		
+		int tlen = 0;
+		int count = 0;
 		oxug = xug = (struct xunpgen *)buf;
 		for (xug = (struct xunpgen *)((char *)xug + xug->xug_len);
 		     xug->xug_len > sizeof(struct xunpgen);
-		     xug = (struct xunpgen *)((char *)xug + xug->xug_len)) {
-			xunp = (struct xunpcb *)xug;
+		     xug = (struct xunpgen *)((char *)xug + xug->xug_len))
+		{
+			xunp = (struct xunpcb64 *)xug;
 			so = &xunp->xu_socket;
 			
+			count+=1;
+			tlen += xunp->xu_len;
+			
+//			printf("len: %u\n", xunp->xu_len);
+//			printf("size: %lu\n", sizeof(struct xunpcb));
+//			continue;
+			
 			/* Ignore PCBs which were freed during copyout. */
-			if (xunp->xu_unp.unp_gencnt > oxug->xug_gen)
-				continue;
+//			if (xunp->xu_unpp.unp_gencnt > oxug->xug_gen)
+//				continue;
 			unixdomainpr(xunp, so);
 		}
 		if (xug != oxug && xug->xug_gen != oxug->xug_gen) {
@@ -131,6 +158,7 @@ int main (int argc, const char * argv[])
 					   socktype[type]);
 			}
 		}
+		printf("tlen: %i, count: %i\n", tlen, count);
 		free(buf);
 	}
 	
--- netstat.h
+++ netstat.h
@@ -31,21 +31,51 @@
 
 LIST_HEAD(unp_head, unpcb);
 
+struct lock_object {
+	   const   char *lo_name;          /* Individual lock name. */
+	         u_int   lo_flags;
+	         u_int   lo_data;                /* General class specific data. */
+	         struct  witness *lo_witness;    /* Data for witness. */
+	};
+
+struct mtx {
+	         struct lock_object      lock_object;    /* Common lock properties. */
+	         volatile uintptr_t      mtx_lock;       /* Owner and flags. */
+	};
+
 struct unpcb {
 	LIST_ENTRY(unpcb) unp_link;     /* glue on list of all PCBs */
 	struct  socket *unp_socket;     /* pointer back to socket */
+//	struct  file *unp_file;         /* back-pointer to file for gc. */
 	struct  vnode *unp_vnode;       /* if associated with file */
 	ino_t   unp_ino;                /* fake inode number */
 	struct  unpcb *unp_conn;        /* control block of connected socket */
-	struct  unp_head unp_refs;      /* referencing socket linked list */
+		struct  unp_head unp_refs;      /* referencing socket linked list */
 	LIST_ENTRY(unpcb) unp_reflink;  /* link in unp_refs list */
 	struct  sockaddr_un *unp_addr;  /* bound address of socket */
 	int     unp_cc;                 /* copy of rcv.sb_cc */
 	int     unp_mbcnt;              /* copy of rcv.sb_mbcnt */
 	unp_gen_t unp_gencnt;           /* generation count of this instance */
-	int     unp_flags;              /* flags */
-	struct  xucred unp_peercred;    /* peer credentials, if applicable */
+//	short   unp_flags;              /* flags */
+//	short   unp_gcflag;             /* Garbage collector flags. */
+//	struct  xucred unp_peercred;    /* peer credentials, if applicable */
+//	u_int   unp_refcount;
+//	u_int   unp_msgcount;           /* references from message queue */
+//	struct  mtx unp_mtx;            /* mutex */
 };
+
+struct  unpcb_oldest {
+	struct  socket *unp_socket;     /* pointer back to socket */
+	struct  vnode *unp_vnode;       /* if associated with file */
+	ino_t   unp_ino;                /* fake inode number */
+	struct  unpcb *unp_conn;        /* control block of connected socket */
+	struct  unpcb *unp_refs;        /* referencing socket linked list */
+	struct  unpcb *unp_nextref;     /* link in unp_refs list */
+	struct  mbuf *unp_addr;         /* bound address of socket */
+	int     unp_cc;                 /* copy of rcv.sb_cc */
+	int     unp_mbcnt;              /* copy of rcv.sb_mbcnt */
+};
+
 
 struct xunpcb {
 	size_t  xu_len;                 /* length of this structure */
@@ -63,4 +93,41 @@ struct xunpcb {
 #define xu_caddr xu_cau.xuu_caddr
 	struct  xsocket xu_socket;
 	u_quad_t        xu_alignment_hack;
+};
+
+
+
+
+
+
+struct xunpcb64_list_entry {
+	u_int64_t   le_next;
+	u_int64_t   le_prev;
+};
+
+struct xunpcb64 {
+	u_int32_t                       xu_len;         /* length of this structure */
+	u_int64_t                       xu_unpp;        /* to help netstat, fstat */
+	struct xunpcb64_list_entry      xunp_link;      /* glue on list of all PCBs */
+	u_int64_t                       xunp_socket;    /* pointer back to socket */
+	u_int64_t                       xunp_vnode;     /* if associated with file */
+	u_int64_t                       xunp_ino;       /* fake inode number */
+	u_int64_t                       xunp_conn;      /* control block of connected socket */
+	u_int64_t                       xunp_refs;      /* referencing socket linked list */
+	struct xunpcb64_list_entry      xunp_reflink;   /* link in unp_refs list */
+	int                             xunp_cc;                /* copy of rcv.sb_cc */
+	int                             xunp_mbcnt;     /* copy of rcv.sb_mbcnt */
+	unp_gen_t                       xunp_gencnt;    /* generation count of this instance */
+	int                             xunp_flags;     /* flags */
+	union {
+		struct sockaddr_un              xuu_addr;
+		char                            xu_dummy1[256];
+	}                               xu_au;          /* our bound address */
+#define xunp_addr xu_au.xuu_addr
+	union {
+		struct sockaddr_un              xuu_caddr;
+		char                            xu_dummy2[256];
+	}                               xu_cau;         /* their bound address */
+#define xunp_caddr xu_cau.xuu_caddr
+	struct xsocket64        xu_socket;
 };
