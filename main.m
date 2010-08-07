2cddd3aafce734b18be16973768e4feb0c9d0b70
Anton Vaynshtok <avaynshtok@gmail.com>
2010-08-06 20:21:46 -0700

    trying self-reference

--- main.m
+++ main.m
@@ -8,7 +8,51 @@
 
 #import <Cocoa/Cocoa.h>
 
+void launchGUI() {	
+	AuthorizationRef auth = NULL;
+	OSStatus err;
+	
+	AuthorizationItem myItems = {kAuthorizationRightExecute, 0, NULL, 0};
+	AuthorizationRights myRights = {1, &myItems};
+	//myRights.count = sizeof (myItems) / sizeof (myItems[0]);
+	//myRights.items = myItems;	
+	
+	AuthorizationFlags myFlags;
+	myFlags = kAuthorizationFlagDefaults |	kAuthorizationFlagInteractionAllowed | kAuthorizationFlagExtendRights | kAuthorizationFlagPreAuthorize;
+	
+	err = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, myFlags, &auth);
+	if ( err != errAuthorizationSuccess ) {
+		NSLog(@"authorizationcreate failed");
+	}
+	
+	// get app path
+	CFBundleRef bundle = CFBundleGetMainBundle();
+	CFStringRef toolName = (CFStringRef) @"netusage";
+	char *toolPath = (char *)malloc(10000);
+	CFURLRef nethogsUrl = CFBundleCopyAuxiliaryExecutableURL(bundle, toolName);
+	CFURLGetFileSystemRepresentation(nethogsUrl, true, (UInt8 *) toolPath, 10000);
+	
+	NSLog(@"toolPath: %s", toolPath);
+	char *arg = "--do-gui";
+
+	//char *abc = "23";
+	const char *args[2];
+	args[0] = toolPath;
+	args[1] = arg;
+	
+	// call the tool
+	err = AuthorizationExecuteWithPrivileges(auth, toolPath, kAuthorizationFlagDefaults, &arg, NULL);
+	//NSLog(@"err: %@", err);
+}
+
 int main(int argc, char *argv[])
 {
-    return NSApplicationMain(argc,  (const char **) argv);
+	//NSLog(@"num args: %i", argc);
+	//if (argc == 2) { // && strcmp(argv[1], "--do-gui") == 0) {
+		return NSApplicationMain(argc,  (const char **) argv);
+	//}		
+//	else {
+//		launchGUI();
+//		exit(1);
+//	}
 }
